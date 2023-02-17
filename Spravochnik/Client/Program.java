bc6ed57b997147fad81391a321e63dd35aec3ec6
mila <milavoronina1997@mail.ru>
2023-02-17 11:54:58 +0500

    hw5

---
+++ Spravochnik/Client/Program.java
@@ -0,0 +1,9 @@
+package HW5.Spravochnik.Client;
+
+import HW5.Spravochnik.UI.App;
+
+public class Program {
+    public static void main(String[] args) {
+        App.start();
+    }
+}
--- Spravochnik/UI/App.java
+++ Spravochnik/UI/App.java
@@ -1,20 +1,26 @@
 package HW5.Spravochnik.UI;
 
-import java.util.Scanner;
+
 
 import HW5.Spravochnik.Core.ExportImport.ExportImportFile;
 import HW5.Spravochnik.Core.Infrastructure.Phonebook;
 import HW5.Spravochnik.Core.Infrastructure.PhonebookMap;
 import HW5.Spravochnik.Core.MVP.Model;
+import HW5.Spravochnik.Core.MVP.Presenter;
+import HW5.Spravochnik.Core.MVP.View;
 
 public class App {
 
     public static void start(){
-        Scanner scanner = new Scanner(System.in);
         Phonebook phonebook = new PhonebookMap();
         ExportImportFile file = new ExportImportFile("phonebook.txt", phonebook);
-        Model model = new Model(file);
-        
+        Model model = new Model(phonebook,file);
+        View view = new ConsoleView();
+        Presenter presenter = new Presenter(model, view);
+        presenter.load();
+        presenter.getMenu();
+        presenter.save();
+
     }
     
 }
